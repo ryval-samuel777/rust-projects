4df307b33b650abd30334e2ada390989aaf9c531
Ryval Samuel <ryvalonibala026@student.unsrat.ac.id>
2023-12-23 23:29:02 +0800

    feat: added code for decompression

--- extract-zip-file-project-2/src/main.rs
+++ extract-zip-file-project-2/src/main.rs
@@ -1,3 +1,83 @@
+extern crate zip;
+
+use zip::ZipArchive;
+use std::fs;
+use std::io;
+use std::process::exit;
+use std::env::args;
+use std::path::Path;
+
+
 fn main() {
-    println!("Hello, world!");
+    exit(decompressed_zip());
+}
+
+fn decompressed_zip () -> i32 {
+    let args: Vec<_> = args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        return 1;
+    }
+    
+    let file_name = Path::new(&*args[1]);
+
+    let file = fs::File::open(&file_name).unwrap();
+
+    let mut archive = ZipArchive::new(file).unwrap();
+    
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(1).unwrap();
+
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        {
+            // debug or checking log
+            let comment = file.comment();
+            if !comment.is_empty(){
+                println!(
+                    "File {} comment:{}", 
+                    i,
+                    comment
+                )
+            }
+        }
+        
+        if (*file.name()).ends_with('/') {
+            println!(
+                "File {} extracted to \"{}\"", 
+                i, 
+                outpath.display()
+            );
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)", 
+                i, 
+                outpath.display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists(){
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = 
+                fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap(); 
+        }
+    
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode(){
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
+    }
+    0
 }
