d7fb4fd893ef14fff09fd1d0e1e747d569493643
Ryval Samuel <ryvalonibala026@student.unsrat.ac.id>
2023-12-26 15:49:52 +0800

    fix(main): implement idiomatic for Article

--- rust-read-json-project-4/src/main.rs
+++ rust-read-json-project-4/src/main.rs
@@ -13,6 +13,13 @@ struct Article {
     paragraph: Vec<Paragraph>
 }
 
+impl Article {
+    fn new(raw_json: &str) -> Article {
+        let parsed = serde_json::from_str(raw_json).unwrap();
+        parsed
+    } 
+}
+
 fn main(){
     let json = r#" 
     {
@@ -32,12 +39,12 @@ fn main(){
         ]
     }"#;
 
-    let parsed: Article = read_json_typed(json);
-    println!("\n\n The name of the first paragraph is : {}", parsed.paragraph[0].name);
+    let parsed = Article::new(json);
+    println!("\n\n The name of the first paragraph is : {}", parsed.paragraph[2].name);
 
 }   
 
-fn read_json_typed(raw_json: &str) -> Article {
-    let parsed: Article = serde_json::from_str(raw_json).unwrap();
-    parsed
-}
+//fn read_json_typed(raw_json: &str) -> Article {
+//    let parsed: Article = serde_json::from_str(raw_json).unwrap();
+//    parsed
+//}
